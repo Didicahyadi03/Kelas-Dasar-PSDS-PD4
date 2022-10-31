5819448aa50c94914f07fd4737dfdf928760bb04
Didicahyadi03 <115863514+Didicahyadi03@users.noreply.github.com>
2022-10-31 20:47:57 +0700

    Add files via upload

---
+++ P1_Data Definition Language & Data Manupulation Language.sql
@@ -0,0 +1,16 @@
+--cara membuat table sendiri
+CREATE TABLE cahyadi (
+NIM INTERGER PRIMARY KEY,
+NamaDepan VARCHAR(10),
+NamaBelakang VARCHAR(10),
+NomorHP VARCHAR(14)
+)
+
+--cara input data
+INSERT INTO cahyadi(NIM,NamaDepan,NamaBelakang,NomorHP)
+VALUES (2200015012,'didi','cahayadi','083824034448')
+
+--cara melihat data yang sudah di inputkan
+SELECT * FROM cahyadi
+
+--tugas insertkan nama teman kalian
